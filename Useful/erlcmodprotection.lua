b18781b53aa0203c96e8e9bf43d0469ee4cf848d
PeachBoi <109909621+spooderman11@users.noreply.github.com>
2022-10-04 07:43:07 +0300

    Create erlcmodprotection.lua

---
+++ Useful/erlcmodprotection.lua
@@ -0,0 +1,126 @@
+while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 91009685  then      ---------- trxistaa (Username)
+           game.Players.LocalPlayer:MakeNotification("Script Owner Has Joined")
+        
+        end;
+      end;
+  end;
+
+
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 452410432  then      ---------- EditSerious (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 406165948  then      ---------- HydroAtoms (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 121361029  then      ---------- Cesstial (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 1115670664  then      ---------- JomoTheBobo (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 1079064859  then      ---------- MrJake_v8 (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 75669309  then      ---------- FLuXSniper (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 168691872  then      ---------- flat_bird (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 25787043  then      ---------- Law_Master (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 69778525  then      ---------- KeyserVFD (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 31006826  then      ---------- WaterLostic (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 52709422  then      ---------- EnchantedG19 (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 50972013  then      ---------- Zethome (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
+
+  while wait() do 
+    for i, v in ipairs(game.Players:GetChildren()) do
+        if v.UserId == 77573259  then      ---------- sli_ckk (Username)
+           game.Players.LocalPlayer:Kick("Kicked : Mod Has Joined.")
+        end;
+      end;
+  end;
